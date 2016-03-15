48cde5db59d53b44ab2661ebf40a3c0798172630
Katrina Rosales <katrinarosales@live.com>
2016-03-15 09:41:06 +0100

    Execercise 22

---
+++ Execercise 22/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execercise_22
+{
+    
+
+    class Program
+    {
+
+        struct Coordinate
+        {
+            public int X;
+            public int Y;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Coordinate c = new Coordinate();
+            c.X = 5;
+            c.Y = 5;
+
+            Coordinate cc = new Coordinate();
+            cc.X = 20;
+            cc.Y = 20;
+
+            Console.WriteLine($"c.X = {c.X}, c.Y = {c.Y}");
+            Console.WriteLine($"cc.X = {cc.X}, cc.Y = {cc.Y}");
+
+            //Console.WriteLine(c.X + " " + c.Y);
+            //Console.WriteLine(cc.X + " "+ cc.Y);
+        }
+
+
+    }
+}
